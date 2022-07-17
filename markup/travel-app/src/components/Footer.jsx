6be8532e3f7b9d6021a416d7d4c2032c1cc52c10
Laptop <babalielchin@gmail.com>
2022-07-17 21:58:37 +0400

    base of the project, header and footer

---
+++ markup/travel-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <span className="footer__text">
+                from <a className="footer__link" href="https://binary-studio.com">binary studio</a> with
+                <img className="footer__icon" src="./assets/images/heart.svg" alt="heart icon" />
+            </span>
+        </footer>
+    );
+};
+
+export default Footer;
